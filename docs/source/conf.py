2a38c001b8166d84ae3889abe7db8fcae104118a
dab-work <bueelerd@ethz.ch>
2026-01-20 16:25:44 +0100

    changed to shibuya theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,13 +5,11 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'shibuya'
 html_title = 'Lego-like building blocks for FEM'
 html_logo = '../../assets/logo.png'
 html_favicon = '../../assets/favicon.png'
 
 html_theme_options = {
-    "repository_url": "https://gitlab.ethz.ch/smec/software/tatva",
-    "use_repository_button": True,
-    "use_source_button": True,
+    "accent_color": "orange",
 }
